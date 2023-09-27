9a2927ed47e1ba61d999f2399762bc8cab04a4bb
conner-huf <conner.huf@gmail.com>
2023-09-27 13:39:47 -0700

    implemented player controls and functionality

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -7,6 +7,8 @@ import SidebarItem from "./SidebarItem";
 import Box from "./Box";
 import Library from "./Library";
 import { Song } from "@/types";
+import usePlayer from "@/hooks/usePlayer";
+import { twMerge } from "tailwind-merge";
 
 interface SidebarProps {
     children: React.ReactNode;
@@ -18,6 +20,7 @@ const Sidebar: React.FC<SidebarProps> = ({
     songs
 }) => {
     const pathname = usePathname();
+    const player = usePlayer();
 
     const routes = useMemo(() => [
         {
@@ -35,7 +38,12 @@ const Sidebar: React.FC<SidebarProps> = ({
     ], [pathname]);
 
     return(
-        <div className="flex h-full">
+        <div className={twMerge(`
+            flex
+            h-full
+        `,
+            player.activeId && "h-[calc(100%-80px)]"
+        )}>
             <div className="
             hidden
             md:flex
